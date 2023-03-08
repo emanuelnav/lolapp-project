8ce9c1dfbfaa3b4485d2c8efb82a3ce180113e25
emanuelnav <emanuelnavarro.dev@gmail.com>
2023-03-07 22:36:02 -0300

    changed api requests to use requests library instead of riotwatcher, functions and validations to obtain summoner data added

--- app.py
+++ app.py
@@ -1,13 +1,29 @@
 from flask import Flask, render_template, request
-from riotwatcher import LolWatcher, ApiError
 from decouple import config
-
+import requests
 
 app = Flask(__name__)
 
-# global variables
+# Global Variables
 api_key = config('API_KEY')
-watcher = LolWatcher(api_key)
+
+def getSummoner(region, username):
+    url = f"https://{region}.api.riotgames.com/lol/summoner/v4/summoners/by-name/{username}"
+    headers = {"X-Riot-Token": api_key}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+      return response.json()
+    else:
+      return []
+
+def getAllLeagues(region, summonerId):
+  url = f"https://{region}.api.riotgames.com/lol/league/v4/entries/by-summoner/{summonerId}"
+  headers = {"X-Riot-Token": api_key}
+  response = requests.get(url, headers=headers)
+  if response.status_code == 200:
+    return response.json()
+  else:
+    return []
 
 @app.route("/")
 def home():
@@ -16,6 +32,7 @@ def home():
 
 @app.route('/summoner')
 def summoner():
-  me = watcher.summoner.by_name(request.args['region'], request.args['summoner-name'])
-  my_ranked_stats = watcher.league.by_summoner(request.args['region'], me['id'])
+  summoner_name = getSummoner(request.args['region'], request.args['summoner-name'])
+  my_ranked_stats = getAllLeagues(request.args['region'], summoner_name['id']) if summoner_name else []
+  print(my_ranked_stats)
   return render_template("invocador.html", ranked_stats=my_ranked_stats)

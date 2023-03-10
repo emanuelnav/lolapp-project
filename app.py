835f19dbb35170b4af80c1a6a2b625681abce60a
emanuelnav <emanuelnavarro.dev@gmail.com>
2023-03-10 18:19:52 -0300

    api requests functions moved to a separate file

--- app.py
+++ app.py
@@ -1,29 +1,9 @@
 from flask import Flask, render_template, request
 from decouple import config
+from my_api import getSummoner, getAllLeagues
 import requests
 
 app = Flask(__name__)
-
-# Global Variables
-api_key = config('API_KEY')
-
-def getSummoner(region, username):
-    url = f"https://{region}.api.riotgames.com/lol/summoner/v4/summoners/by-name/{username}"
-    headers = {"X-Riot-Token": api_key}
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-      return response.json()
-    else:
-      return []
-
-def getAllLeagues(region, summonerId):
-  url = f"https://{region}.api.riotgames.com/lol/league/v4/entries/by-summoner/{summonerId}"
-  headers = {"X-Riot-Token": api_key}
-  response = requests.get(url, headers=headers)
-  if response.status_code == 200:
-    return response.json()
-  else:
-    return []
 
 @app.route("/")
 def home():
---
+++ my_api.py
@@ -0,0 +1,22 @@
+from decouple import config
+import requests
+
+api_key = config('API_KEY')
+
+def getSummoner(region, username):
+    url = f"https://{region}.api.riotgames.com/lol/summoner/v4/summoners/by-name/{username}"
+    headers = {"X-Riot-Token": api_key}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+      return response.json()
+    else:
+      return []
+
+def getAllLeagues(region, summonerId):
+  url = f"https://{region}.api.riotgames.com/lol/league/v4/entries/by-summoner/{summonerId}"
+  headers = {"X-Riot-Token": api_key}
+  response = requests.get(url, headers=headers)
+  if response.status_code == 200:
+    return response.json()
+  else:
+    return []

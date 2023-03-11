9f09deb05a20c7dba7465deb030057b40c6399a7
RamirezFacundo <facundoramirez32@gmail.com>
2023-03-11 02:45:07 -0300

    Graphic Changes

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from decouple import config
 from my_api import getSummoner, getAllLeagues
 from masteries import getMasteriesChampionsGraph
 import requests
+import os 
 
 app = Flask(__name__)
 
@@ -16,5 +17,6 @@ def summoner():
   summoner_name = getSummoner(request.args['region'], request.args['summoner-name'])
   my_ranked_stats = getAllLeagues(request.args['region'], summoner_name['id']) if summoner_name else []
   champ_masteries_graph = getMasteriesChampionsGraph(request.args['region'], summoner_name['id'])
+  # champ_masteries_graph.savefig(os.path.join('static', 'images', 'graph.png'))
   print(my_ranked_stats)
-  return render_template("invocador.html", ranked_stats=my_ranked_stats, champs=champ_masteries_graph)
+  return render_template("invocador.html", ranked_stats=my_ranked_stats)
--- masteries.py
+++ masteries.py
@@ -1,8 +1,10 @@
 import pandas as pd
 from my_api import *
-import requests
+# import matplotlib
+# matplotlib.use('Agg')
+import os
 import matplotlib.pyplot as plt
-import numpy as np
+plt.switch_backend('agg')
 
 def getMasteriesChampionsGraph(region, summonerId):
     champion_masteries = getChampionMasteries(region, summonerId)
@@ -28,12 +30,36 @@ def getMasteriesChampionsGraph(region, summonerId):
     # plt.show()
 
 
-    # plt.bar(x_values, y_values)          #El gráfico
-    # plt.title('Champion y nivel')        #El título
-    # ax = plt.subplot()                   #Axis
-    # ax.set_xticks(x_values)              #Eje x
-    # ax.set_xticklabels(x_values)         #Etiquetas del eje x
-    # ax.set_xlabel('Campeon')             #Nombre del eje x
-    # ax.set_ylabel('level')               #Nombre del eje y
-    # plt.show()
-    return champion_names
+
+    df = pd.DataFrame(champion_masteries)
+    #print(df.head())
+
+    x_values = df['championId'].head()
+    y_values = df['championPoints'].head()
+
+    champion_level = y_values.values.tolist()
+    champion_ids = x_values.values.tolist()
+    champion_ids_str = [str(x) for x in champion_ids]
+
+    champion_names = []
+    for i in champion_ids_str:
+        champion_names.append(all_champs_name[i])
+    print(champion_names)
+
+
+
+    ### GRAFICOS 
+
+    plt.bar(champion_names, champion_level, width=0.5, color=['#5c58b6','#b457ce','#5994ce','#3a4e93'])
+    plt.title('Maestria de campeon mas alta')
+    plt.xlabel('Campeones')
+    plt.ylabel('Puntos')
+    plt.style.use('seaborn-darkgrid')
+    #plt.show()
+    plt.savefig(os.path.join('static', 'images', 'graph.png'))
+    return plt
+# summoner_name = getSummoner('la2','krosz')
+# getMasteriesChampionsGraph('la2',summoner_name['id'])
+
+
+
